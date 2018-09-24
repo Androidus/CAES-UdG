daba14301008fa3585c4bd8455056b3b04cc4d11
Andreu Vila <u1933593@campus.udg.edu>
2018-09-24 18:17:59 +0200

    Exercici fet

--- lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
+++ lab/junithelloworld/src/test/java/edu/udg/caes/ExercicesIntroTestingTest.java
@@ -29,4 +29,28 @@ public class ExercicesIntroTestingTest {
         final int result = ExercicesIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
     }
+
+    @Test
+    public void testFindLast2(){
+        final int result = ExercicesIntroTesting.findLast(new int []{2, 3, 5}, 3);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void testLastZero2(){
+        final int result = ExercicesIntroTesting.lastZero(new int []{2, 1, 0});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testCountPositive2(){
+        final int result = ExercicesIntroTesting.countPositive(new int []{-4, 2, -1, 2});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testOddOrPos2(){
+        final int result = ExercicesIntroTesting.oddOrPos(new int []{3, -2, 0, 1, 4});
+        assertEquals(result, 3);
+    }
 }
